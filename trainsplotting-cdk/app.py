99157d8647a9c4dc2d5cbe1e28de13ac1d6e7010
Andrew Eaves <andrew@eavesofit.com>
2019-08-11 13:09:38 -0400

    Updated app.py to relect correct region and account

--- trainsplotting-cdk/app.py
+++ trainsplotting-cdk/app.py
@@ -6,6 +6,6 @@ from trainsplotting_cdk.trainsplotting_cdk_stack import TrainsplottingCdkStack
 
 
 app = core.App()
-TrainsplottingCdkStack(app, "trainsplotting-cdk")
+TrainsplottingCdkStack(app, "trainsplotting-cdk", env={ account: '650457438316', 'region': 'us-east-2'})
 
 app.synth()
