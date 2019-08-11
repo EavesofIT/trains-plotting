135646ee7fd089e4de8907a578a459a3954a87dd
Andrew Eaves <andrew@eavesofit.com>
2019-08-11 13:49:02 -0400

    Added code to create lambda function

--- trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
+++ trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
@@ -1,5 +1,8 @@
 from aws_cdk import (
     aws_s3 as s3,
+    aws_events as events,
+    aws_lambda as lambda_,
+    aws_events_targets as targets,
     core
 )
 
@@ -14,3 +17,15 @@ class TrainsplottingCdkStack(core.Stack):
             encryption=s3.BucketEncryption.S3_MANAGED,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
+
+        with open("photo-ingestion.py", encoding="utf8") as fp:
+            photoingestion_code = fp.read()
+        
+        lambdaFn = lambda_.Function(
+            self, "photo-ingestion",
+            code=lambda_.InlineCode(photoingestion_code),
+            handler="index.main",
+            timeout=core.Duration.seconds(300),
+            runtime=lambda_.Runtime.PYTHON_3_7,
+        )
+        
