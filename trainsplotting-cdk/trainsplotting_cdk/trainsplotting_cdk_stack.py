479c701c87ca96628305c4ef8d296a7ba2c10d11
Andrew Eaves <andrew@eavesofit.com>
2019-08-11 19:15:03 -0400

    Updated stack to build out more functions and topics I need to figure out how to subscribe a lambda function to an SNS topic line 64

--- trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
+++ trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
@@ -15,6 +15,7 @@ class TrainsplottingCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Create bucket for image uploads
         bucket = s3.Bucket(self, 
             "trainsplotting-ingestion",
             encryption=s3.BucketEncryption.S3_MANAGED,
@@ -28,7 +29,8 @@ class TrainsplottingCdkStack(core.Stack):
         with open("lambda_handlers/photo-ingestion.py", encoding="utf8") as fp:
             photoingestion_code = fp.read()
         
-        lambdaFn = lambda_.Function(
+        # Creation of lambda function to process new images and kick of rekognition
+        photoIngestFn = lambda_.Function(
             self, "photo-ingestion",
             #code=lambda_.cfn_parameters or lambda_.bucket may need to be used if code is bigger than 4KiB
             code=lambda_.InlineCode(photoingestion_code),
@@ -36,6 +38,27 @@ class TrainsplottingCdkStack(core.Stack):
             timeout=core.Duration.seconds(300),
             runtime=lambda_.Runtime.PYTHON_3_7,
         )
-        
-        lambdaFn.add_event_source(lambdaevents.S3EventSource(bucket,events=[s3.EventType.OBJECT_CREATED]))
-        
+        # Adding S3 event notification for Lambda function
+        photoIngestFn.add_event_source(lambdaevents.S3EventSource(bucket,events=[s3.EventType.OBJECT_CREATED]))
+
+        # SNS Topic for Rekognition to publish to when completed
+        resultstopic = sns.Topic(
+            display_name='rekognition-results-topic'
+        )
+
+        # Read code for processing rekognition results
+        with open("lambda_handlers/process-rekog-results.py", encoding="utf8") as fp:
+            rekogresults_code = fp.read()
+
+        # Creation of lambda function to process results of rekognition
+        rekogResultsFn = lambda_.Function(
+            self, "rekognition-results",
+            #code=lambda_.cfn_parameters or lambda_.bucket may need to be used if code is bigger than 4KiB
+            code=lambda_.InlineCode(rekogresults_code),
+            handler="index.main",
+            timeout=core.Duration.seconds(300),
+            runtime=lambda_.Runtime.PYTHON_3_7,
+        )
+
+        # Subscribe the lambda function to the SNS topic that Rekognition will use to publish finished results
+        photoIngestFn.add_event_source(lambdaevents.SnsEventSource(resultstopic))
