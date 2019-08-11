0ef3cdc7445913adda073ff08373d527b544e7ff
Andrew Eaves <andrew@eavesofit.com>
2019-08-11 13:22:08 -0400

    Updated stack to encrypt at bucket level

--- trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
+++ trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as s3,
+    core
+)
 
 
 class TrainsplottingCdkStack(core.Stack):
@@ -6,4 +9,8 @@ class TrainsplottingCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, 
+            "trainsplotting-ingestion",
+            bucket_name="trainsplotting-ingestion",
+            encryption=s3.BucketEncryption.S3_MANAGED,
+        )
