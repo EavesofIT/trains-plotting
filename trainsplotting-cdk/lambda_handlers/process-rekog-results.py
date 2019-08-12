f8f460fc7c432aa521898b312343550a42c3995b
Andrew Eaves <andrew@eavesofit.com>
2019-08-12 17:30:36 -0400

    Added notes for how to handle the AAR serial number matching

--- trainsplotting-cdk/lambda_handlers/process-rekog-results.py
+++ trainsplotting-cdk/lambda_handlers/process-rekog-results.py
@@ -2,3 +2,5 @@ def main(event, context):
     print("I'm running!")
     print("Add code to process Rekognition results and tag pictures!")
     print(event)
+    # Remove spaces from the text when validating regex to see if it is the serial number
+    # regex = \w{1,4}\W\d{4,6}
