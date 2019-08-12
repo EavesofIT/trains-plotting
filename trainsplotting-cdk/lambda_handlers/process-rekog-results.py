260735bf11beefad4927da144fd04cbdd7b75bc5
Andrew Eaves <andrew@eavesofit.com>
2019-08-12 17:46:52 -0400

    Updated process-rekog-results with regex for serial identification

--- trainsplotting-cdk/lambda_handlers/process-rekog-results.py
+++ trainsplotting-cdk/lambda_handlers/process-rekog-results.py
@@ -1,6 +1,16 @@
+import re
+
 def main(event, context):
     print("I'm running!")
     print("Add code to process Rekognition results and tag pictures!")
     print(event)
     # Remove spaces from the text when validating regex to see if it is the serial number
+    # txtAARMarkTextPrepped = txtAARMarkText.replace(" ","")
     # regex = \w{1,4}\W\d{4,6}
+    # p = re.compile('(\w{1,4})(\d{4,6})')
+    # m = p.search(txtAARMarkTextPrepped)
+    # Search value = SHQX50757
+    # m.group(0) = SHQX50757
+    # m.group(1) = SHQX
+    # m.group(2) = 50757
+    # m.group(2)[1] = 0
