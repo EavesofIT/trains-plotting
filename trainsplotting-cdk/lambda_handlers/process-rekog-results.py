b8fd9c488438192fb05663b629536d61c42cca16
Andrew Eaves <andrew@eavesofit.com>
2019-09-02 15:48:16 -0400

    Cleaned up variable names and formatting Added environment variable for dynamodb table name to the rekog_results_fn

--- trainsplotting-cdk/lambda_handlers/process-rekog-results.py
+++ trainsplotting-cdk/lambda_handlers/process-rekog-results.py
@@ -1,9 +1,14 @@
 import re
+import boto3
 
 def main(event, context):
     print("I'm running!")
     print("Add code to process Rekognition results and tag pictures!")
     print(event)
+    #
+    # Put the event over to the DynamoDB
+    #
+
     # Remove spaces from the text when validating regex to see if it is the serial number
     # txtAARMarkTextPrepped = txtAARMarkText.replace(" ","")
     # regex = \w{1,4}\W\d{4,6}
@@ -14,3 +19,13 @@ def main(event, context):
     # m.group(1) = SHQX
     # m.group(2) = 50757
     # m.group(2)[1] = 0
+
+    dynamodb = boto3.client('dynamodb')
+    dynamodb.put_item(
+        TableName='',
+        Item={}
+    )
+    
+    # 
+    # Send the event to the next lambda which should start the deeper analysis for inspection points, after successfully storing in DynamoDB
+    #
--- trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
+++ trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
@@ -17,25 +17,25 @@ class TrainsplottingCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create bucket for image uploads
-        bucket = s3.Bucket(self, 
+        # Create ingestion_bucket for image uploads
+        ingestion_bucket = s3.Bucket(self,
             "trainsplotting-ingestion",
             encryption=s3.BucketEncryption.S3_MANAGED,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL
         )
 
         #**********#
-        # Could add another bucket here and add the zip of the code to use in Lambda functions
+        # Could add another ingestion_bucket here and add the zip of the code to use in Lambda functions
         #**********#
 
         # Read code for processing rekognition results
-        with open("lambda_handlers/process-rekog-results.py", encoding="utf8") as fp:
-            rekogresults_code = fp.read()
+        proc_results_filepath = "lambda_handlers/process-rekog-results.py"
+        with open(proc_results_filepath, encoding="utf8") as file_process_results_process:
+            rekogresults_code = file_process_results_process.read()
 
         # Creation of lambda function to process results of rekognition
-        rekogResultsFn = lambda_.Function(
+        rekog_results_fn = lambda_.Function(
             self, "rekognition-results",
-            #code=lambda_.cfn_parameters or lambda_.bucket may need to be used if code is bigger than 4KiB
             code=lambda_.InlineCode(rekogresults_code),
             handler="index.main",
             timeout=core.Duration.seconds(150),
@@ -44,21 +44,21 @@ class TrainsplottingCdkStack(core.Stack):
         )
 
         # SNS Topic for Rekognition to publish to when completed
-        resultstopic = sns.Topic(self,'rekognition-results-topic',
+        results_topic = sns.Topic(self,'rekognition-results-topic',
             display_name='rekognition-results-topic'
         )
 
         # Subscribe the lambda function to the SNS topic that Rekognition will use to publish finished results
-        rekogResultsFn.add_event_source(lambdaevents.SnsEventSource(resultstopic))
+        rekog_results_fn.add_event_source(lambdaevents.SnsEventSource(results_topic))
 
+        proc_photo_ingestion_filepath = "lambda_handlers/photo-ingestion.py"
+        with open(proc_photo_ingestion_filepath, encoding="utf8") as file_process_photo_ingestion:
+            photoingestion_code = file_process_photo_ingestion.read()
 
-        with open("lambda_handlers/photo-ingestion.py", encoding="utf8") as fp:
-            photoingestion_code = fp.read()
-        
         # Creation of lambda function to process new images and kick of rekognition
-        photoIngestFn = lambda_.Function(
+        photo_ingest_fn = lambda_.Function(
             self, "photo-ingestion",
-            #code=lambda_.cfn_parameters or lambda_.bucket may need to be used if code is bigger than 4KiB
+            #code=lambda_.cfn_parameters or lambda_.ingestion_bucket may need to be used if code is bigger than 4KiB
             code=lambda_.InlineCode(photoingestion_code),
             handler="index.main",
             timeout=core.Duration.seconds(150),
@@ -66,33 +66,37 @@ class TrainsplottingCdkStack(core.Stack):
             memory_size=256
         )
         # Add environment variable for the SNS Topic
-        photoIngestFn.add_environment(key='TOPIC_ARN',value=resultstopic.topic_arn)
+        photo_ingest_fn.add_environment(key='TOPIC_ARN',value=results_topic.topic_arn)
         # Adding S3 event notification for Lambda function
-        photoIngestFn.add_event_source(lambdaevents.S3EventSource(bucket,events=[s3.EventType.OBJECT_CREATED]))
+        photo_ingest_fn.add_event_source(lambdaevents.S3EventSource(ingestion_bucket,events=[s3.EventType.OBJECT_CREATED]))
         # Add permission to publish to SNS for processing results
-        photoIngestFn.add_to_role_policy(iam.PolicyStatement(actions=['sns:Publish'],resources=[resultstopic.topic_arn]))
-        # Add permission to get object from the bucket
-        bucketObjectsPath = bucket.bucket_arn + "/*"
-        photoIngestFn.add_to_role_policy(iam.PolicyStatement(actions=['s3:GetObject'],resources=[bucketObjectsPath]))
-        photoIngestFn.add_to_role_policy(iam.PolicyStatement(actions=['rekognition:DetectText','rekognition:DetectLabels','rekognition:DetectModerationLabels'],resources=['*']))
+        photo_ingest_fn.add_to_role_policy(iam.PolicyStatement(actions=['sns:Publish'],resources=[results_topic.topic_arn]))
+        # Add permission to get object from the ingestion_bucket
+        bucket_objects_path = ingestion_bucket.bucket_arn + "/*"
+        photo_ingest_fn.add_to_role_policy(iam.PolicyStatement(actions=['s3:GetObject'],resources=[bucket_objects_path]))
+        photo_ingest_fn.add_to_role_policy(iam.PolicyStatement(actions=['rekognition:DetectText','rekognition:DetectLabels','rekognition:DetectModerationLabels'],resources=['*']))
 
         # Create DynamoDB table to contain JSON body returned by Rekognition
-        partionKeySerialNumber = dynamo.Attribute(
+        partion_key_serial_number = dynamo.Attribute(
             name="serial_number",
             type=dynamo.AttributeType.STRING
         )
-        dynamo.Table(self, "rekogitionResultsDB",
-            partition_key=partionKeySerialNumber
+        rekog_results_table = dynamo.Table(self, "rekogitionResultsDB",
+            partition_key=partion_key_serial_number
         )
         
-        # Add permission for the Lambda function to 
-        rekogResultsFn.add_to_role_policy(iam.PolicyStatement(actions=[
-				"dynamodb:BatchGetItem",
-				"dynamodb:GetItem",
-				"dynamodb:Query",
-				"dynamodb:Scan",
-				"dynamodb:BatchWriteItem",
-				"dynamodb:PutItem",
-				"dynamodb:UpdateItem"
-			],resources=[rekogResultsFn.function_arn])
+        # Add permission for the Lambda function to putitems in the DynamoDB used to store the results from Rekog
+        rekog_results_fn.add_to_role_policy(iam.PolicyStatement(
+            actions=[
+                #"dynamodb:BatchGetItem",
+                #"dynamodb:GetItem",
+                #"dynamodb:Query",
+                #"dynamodb:Scan",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:PutItem"
+                #"dynamodb:UpdateItem"
+            ],
+            resources=[rekog_results_fn.function_arn])
         )
+
+        rekog_results_fn.add_environment(key='DYNAMODB_NAME', value=rekog_results_table.table_name)
