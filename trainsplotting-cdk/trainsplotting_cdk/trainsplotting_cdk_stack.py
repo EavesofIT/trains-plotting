1ad6cda8f7c8f58a9fc768e9716de6dfa2e6c6f9
AJ Eaves <andrew@eavesofit.com>
2019-10-10 12:14:36 -0400

    Update trainsplotting_cdk_stack.py
    Added permission for ec2 instance profile to read the rds secret info.

--- trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
+++ trainsplotting-cdk/trainsplotting_cdk/trainsplotting_cdk_stack.py
@@ -135,7 +135,6 @@ class TrainsplottingCdkStack(core.Stack):
             virtualization=ec2.AmazonLinuxVirt.HVM,
             storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
         )
-        # Create instance profile with permission to put object to the s3 bucket and get the secret
         
         # Create the trainsplotting app instance
         trainsplotting_app_ec2 = ec2.Instance(self, "trainsplotting-app",
@@ -146,6 +145,12 @@ class TrainsplottingCdkStack(core.Stack):
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.ISOLATED),
             #role=,
         )
+        
+        # Create instance profile with permission to put object to the s3 bucket and get the secret        
+        railcar_inspection_table.secret.grant_read(trainsplotting_app_ec2.role)
+        
+        # Setup user data to configure environment variables for DB info, DB secret, and S3 info
+        
 
         # Create S3 bucket for Sagemaker Results storage
         sagemaker_results_bucket = s3.Bucket(self,
