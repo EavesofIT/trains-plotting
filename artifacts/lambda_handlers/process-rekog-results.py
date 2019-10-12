fb728b6024e2382cb42eb3ffa882135db3e1bb84
Andrew Eaves <andrew@eavesofit.com>
2019-10-12 17:24:40 -0400

    Updated lambda function to store serial number now

--- artifacts/lambda_handlers/process-rekog-results.py
+++ artifacts/lambda_handlers/process-rekog-results.py
@@ -10,9 +10,21 @@ def main(event, context):
     print(event['Records'][0]['Sns']['Message'])
     textdata = event['Records'][0]['Sns']['Message']
     splittextdata = textdata.split(';')
+    serialnumber = ""
     for text in splittextdata:
         print(text)
-    
+        serialtext = text.replace(" ","")
+        p = re.compile('[a-zA-Z]{2,4}\d{4,6}\\b')
+        m = p.search(serialtext)
+        if m is not None:
+            print(m[0])
+            serialnumber = m[0]
+            # Also check the confidence and ensure about 66
+
+            # Need to handle the use case where there are more than one, they will currently overwrite
+    print ("Serial Below")
+    print(serialnumber)
+
     #
     # Sort Rekognition results by confidence
     # We are looking for a 1-4 letter by 4-6 number "word" or two separate values with high confidence(>90)
